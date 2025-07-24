f37ebcfc9907ed14c14f4e0f136996e8ba3a9afb
Mattia Rosati <mattiarosati98@gmail.com>
2025-07-24 22:36:27 +0200

    warning: improved detail card

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -2,7 +2,9 @@ import { Component, inject } from '@angular/core';
 import { SafeHtml } from '@angular/platform-browser';
 import { IconService } from '../../services/icon.service';
 import { ButtonService } from '../../services/button.service';
-import { CustomButton } from '../../services/models/models';
+import { CustomButton, User } from '../../services/models/models';
+import { Router } from '@angular/router';
+import { environment } from '../../environment/environment';
 
 @Component({
   selector: 'app-navbar',
@@ -14,8 +16,9 @@ export class NavbarComponent {
 
   iconService: IconService = inject(IconService);
   buttonService: ButtonService = inject(ButtonService);
+  router: Router = inject(Router);
 
-  
+  user: User = environment.user
   isMenuOpen: boolean = false;
 
   buttons: CustomButton[] = this.buttonService.buttons
@@ -26,5 +29,9 @@ export class NavbarComponent {
 
   toggleMenu(): void{
     this.isMenuOpen = !this.isMenuOpen
+  }
+
+  navigateToHome(): void{
+    this.router.navigate(['home']);
   }
 }
