5cd0004f22730772bb90264334509739ef57d538
Mattia Rosati <mattiarosati98@gmail.com>
2025-08-17 14:35:01 +0200

    added teext to language button

--- src/app/components/language-button/language-button.component.ts
+++ src/app/components/language-button/language-button.component.ts
@@ -1,16 +1,23 @@
-import { Component, computed, EventEmitter, inject, Output, Signal } from '@angular/core';
+import { Component, computed, EventEmitter, inject, Input, Output, Signal } from '@angular/core';
 import { TranslationService } from '../../services/translation.service';
+import { TranslatePipe } from '../../pipes/translate.pipe';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-language-button',
-  imports: [],
+  imports: [
+    TranslatePipe,
+    CommonModule
+  ],
   templateUrl: './language-button.component.html',
   styleUrl: './language-button.component.css'
 })
 export class LanguageButtonComponent {
 
   translateService: TranslationService = inject(TranslationService);
-  
+
+
+  @Input() showText: boolean = false;
   @Output() onSwitch: EventEmitter<void> = new EventEmitter<void>();
 
   basePath: string = 'assets/lang/'
