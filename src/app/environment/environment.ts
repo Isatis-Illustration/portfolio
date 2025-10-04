9a4dae71384aa0fd6c198b95ce617e723e842945
Mattia Rosati <mattiarosati98@gmail.com>
2025-10-04 08:37:04 +0200

    added pubblications

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -34,28 +34,44 @@ Vorrei sapere di più su come lavori e magari parlare di un’idea che ho in men
         },
         
         description:{
-            it: `Ciao! Sono Isabella, il mio nome artistico è Isatis, un piccolo fiore da cui si estrae un colorante blu, a cui sono affezionata sin da piccola. 
-
-Dopo la laurea in Scienze della Formazione, con indirizzo per l’educazione dell’infanzia, ho deciso di seguire la mia vera vocazione: raccontare storie attraverso le immagini.
+            it: `<b>Chi sono:</b>
+            
+            Ciao! Sono Isabella, mio nome artistico è Isatis, un piccolo fiore da cui si estrae un colorante blu, a cui sono affezionata sin da piccola. Dopo la laurea in Scienze della Formazione, con indirizzo per l’educazione dell’infanzia, ho deciso di seguire la mia vera vocazione: raccontare storie attraverso le immagini.
 
 Attualmente studio animazione alla Scuola Internazionale di Comics a Roma, un percorso che mi sta aiutando a rendere il mio stile più dinamico e narrativo.
 Lavoro con passione su personaggi, ambienti e narrazioni visive, sperimentando tecniche e linguaggi diversi.
 
-Il mio obiettivo è lavorare nell’ambito dell’illustrazione editoriale, con particolare interesse per l’editoria per l’infanzia, poiché credo fermamente che le immagini abbiano un ruolo educativo potente e possano contribuire alla crescita emotiva e cognitiva dei più piccoli.
+Il mio obiettivo è lavorare nell’ambito dell’illustrazione editoriale, con particolare interesse per l’editoria per l’infanzia, poiché credo fermamente che le immagini abbiano un ruolo educativo potente e possano contribuire alla crescita emotiva e cognitiva dei più piccoli.Ad oggi sono aperta a progetti destinati a pubblici e contesti differenti, alla ricerca di opportunità per crescere, collaborare e dare forma a mondi visivi autentici e coinvolgenti.
 
-Ad oggi sono aperta a progetti destinati ad un pubblico e contenuti differenti, alla ricerca di opportunità per crescere, collaborare e dare forma a mondi visivi autentici e coinvolgenti.`,
-    
 
-            en: `Hi! I’m Isabella, and my artist name is Isatis, inspired by a small flower that produces a blue dye, which I’ve loved since childhood. 
+<b>Pubblicazioni:</b><ul><li>
+    &bull; Copertina illustrata | <i>Benvenuti a Trenta Capre, il paese dell'Amilcare e della Pinuccia</i>  
+    &emsp;&emsp;di Cinzia Montagna, Navarra Editore, 2023
+  </li><li>
+    &bull; Copertina illustrata | <i>Bentornati a Trenta Capre</i>  
+    &emsp;&emsp;di Cinzia Montagna, Navarra Editore, in pubblicazione
+  </li>
+</ul>
+`,
             
-After graduating in Educational Sciences with a focus on early childhood, I decided to follow my true passion: telling stories through images.
+            en: `<b>About Me:</b>
+            
+            Hi! I’m Isabella, and my artist name is Isatis, inspired by a small flower that produces a blue dye, which I’ve loved since childhood. After graduating in Educational Sciences with a focus on early childhood, I decided to follow my true passion: telling stories through images.
 
 I’m currently studying animation at the Scuola Internazionale di Comics in Rome, which is helping me make my style more dynamic and narrative. 
 I work with enthusiasm on characters, environments, and visual storytelling, experimenting with different techniques and approaches.
 
-My goal is to work in editorial illustration, especially children’s publishing, as I believe images play a powerful educational role and support emotional and cognitive growth. 
+My goal is to work in editorial illustration, especially children’s publishing, as I believe images play a powerful educational role and support emotional and cognitive growth. I am open to projects for various audiences and contexts, seeking opportunities to grow, collaborate, and create authentic and engaging visual worlds.
 
-I am open to projects for various audiences and contexts, seeking opportunities to grow, collaborate, and create authentic and engaging visual worlds.`
+
+<b>Pubblications:</b><ul><li>
+    &bull; Illustrated Cover | <i>Benvenuti a Trenta Capre, il paese dell'Amilcare e della Pinuccia</i>  
+    &emsp;&emsp;di Cinzia Montagna, Navarra Editore, 2023
+  </li><li>
+    &bull; Illustrated Cover | <i>Bentornati a Trenta Capre</i>  
+    &emsp;&emsp;di Cinzia Montagna, Navarra Editore, in pubblication
+  </li>
+</ul>`
         }
     },
 }
